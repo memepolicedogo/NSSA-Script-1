09efbee0eb0bcac1dc3624a1b51cbb586b92049b
memepolicedogo <finseyconrymurray@gmail.com>
2025-09-03 21:52:20 -0400

    more edit

--- src.asm
+++ src.asm
@@ -6,10 +6,10 @@ section .data
 		db "4. Test DNS Resolution",10
 		db "5. Exit the script",10
 	menuLen		equ $-menu
-	gateFile:	db "/usr/sbin/bash",0
+	gateFile:	db "/usr/bin/bash",0
 	null:		db 0
 	gateArg1:	db "-c",0
-	gateArg2:	db "ip r | grep -m 1 -o -E -e ",34,"[0-9]{1,3}",92,".[0-9]{1,3}",92,".[0-9]{1,3}",92,".[0-9]{1,3}",34
+	gateArg2:	db "/usr/sbin/ip r | grep -m 1 -o -E -e ",34,"[0-9]{1,3}",92,".[0-9]{1,3}",92,".[0-9]{1,3}",92,".[0-9]{1,3}",34
 			db " | grep -m 1 -o -E -e ",34,"[0-9]{1,3}",92,".[0-9]{1,3}",92,".[0-9]{1,3}",92,".[0-9]{1,3}",34,0
 	tst		equ $-gateArg2
 
