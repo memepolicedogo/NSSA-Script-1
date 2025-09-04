02ee3093aa26682912f3caa0fc5a1975bd99c595
memepolicedogo <finseyconrymurray@gmail.com>
2025-09-03 22:37:35 -0400

    Local finalizations

--- src.asm
+++ src.asm
@@ -1,4 +1,7 @@
 section .data
+	shebang:	db "#!"
+	comment:	db "# Samuel Conry-Murray 9/3/2025"
+	clear:		db 27, "[H",27,"[2J",27,"[3J"
 	menu:
 		db "1. Display the default gateway",10
 		db "2. Test Local Connectivity",10
@@ -6,12 +9,16 @@ section .data
 		db "4. Test DNS Resolution",10
 		db "5. Exit the script",10
 	menuLen		equ $-menu
-	gateFile:	db "/usr/bin/bash",0
 	null:		db 0
-	gateArg1:	db "-c",0
-	gateArg2:	db "/usr/sbin/ip r | grep -m 1 -o -E -e ",34,"[0-9]{1,3}",92,".[0-9]{1,3}",92,".[0-9]{1,3}",92,".[0-9]{1,3}",34
-			db " | grep -m 1 -o -E -e ",34,"[0-9]{1,3}",92,".[0-9]{1,3}",92,".[0-9]{1,3}",92,".[0-9]{1,3}",34,0
-	tst		equ $-gateArg2
+	bash:	db "/usr/bin/bash",0
+	arg1:	db "-c",0
+	localArg:	db "ping -c 1 $("
+	gateArg:	db "/usr/sbin/ip r | grep -m 1 -o -E -e ",34,"[0-9]{1,3}",92,".[0-9]{1,3}",92,".[0-9]{1,3}",92,".[0-9]{1,3}",34
+			db " | grep -m 1 -o -E -e ",34,"[0-9]{1,3}",92,".[0-9]{1,3}",92,".[0-9]{1,3}",92,".[0-9]{1,3}",34
+	crucial:	db 0
+			db " &>/dev/null && echo ",34,"Local connection is good",34," || echo ",34,"Local connection is bad",34,0
+	remoteArg:	db "ping -c 1 129.21.3.17 &>/dev/null && echo ",34,"Remote connection is good",34," || echo ",34,"Remote connection is bad",34,0
+	dnsArg:		db "ping -c 1 google.com &>/dev/null && echo ",34,"DNS is working",34," || echo ",34,"DNS is not working",34,0
 
 	; "ip r | grep -m 1 -o -E -e \"[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\" | grep -m 1 -o -E -e \"[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\""
 	
@@ -24,6 +31,16 @@ global _start
 _start:
 	mov	rax, null
 	mov	qword [envp], rax
+	mov	rax, bash
+	mov	qword [argv], rax
+	mov	rax, arg1
+	mov	qword [argv+8], rax
+	mov	rax, 1
+	mov	rdi, 1
+	mov	rsi, clear
+	mov	rdx, 11
+	syscall
+
 	mov	rax, 1
 	mov	rdi, 1
 	mov	rsi, menu
@@ -49,31 +66,31 @@ _start:
 	jmp	exit
 
 gateway:
-	mov	rax, gateFile
-	mov	qword [argv], rax
-	mov	rax, gateArg1
-	mov	qword [argv+8], rax
-	mov	rax, gateArg2
+	mov	rax, gateArg
 	mov	qword [argv+16], rax
+	jmp	exec
+local:
+	mov	rax, localArg
+	mov	qword [argv+16], rax
+	mov	byte [crucial], 41
+	jmp	exec
+
+remote:
+	mov	rax, remoteArg
+	mov	qword [argv+16], rax
+	jmp	exec
+
+dns:
+	mov	rax, dnsArg
+	mov	qword [argv+16], rax
+	jmp	exec
+
+exec:
 	mov	rax, 59
-	mov	rdi, gateFile
+	mov	rdi, bash
 	mov	rsi, argv
 	mov	rdx, envp
 	syscall
-	jmp	exit
-
-local:
-
-	jmp	exit
-
-remote:
-	
-	jmp	exit
-
-dns:
-
-	jmp	exit
-
 
 exit:
 	mov	rax, 60
